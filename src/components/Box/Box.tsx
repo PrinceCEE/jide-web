3f96108f1e03569f5f5fe238ab0f960d24fb3126
Chimezie <princecee15@gmail.com>
2022-10-30 23:59:35 +0100

    added cores for button, text and box

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,6 +1,28 @@
 import { FC } from "react";
 import { BoxType } from "src/types";
 
-export const Box: FC<BoxType> = () => {
-  return <div></div>;
+export const Box: FC<BoxType> = ({
+  children,
+  width = "",
+  height = "",
+  bg = "",
+  border = "",
+  radius = "",
+  className = "",
+  justify = "",
+  align = "",
+  flex = "",
+  flexDirection = "",
+  padding,
+  margin,
+  onClick = () => {},
+}) => {
+  return (
+    <div
+      className={`${width} ${height} ${bg} ${border} ${radius} ${className} ${justify} ${align} ${flex} ${flexDirection} ${margin} ${padding}`}
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  );
 };
--- src/types/components/index.ts
+++ src/types/components/index.ts
@@ -1,12 +1,38 @@
-import { ReactNode } from "react";
+import { ReactNode, MouseEventHandler } from "react";
+import {
+  ALIGN,
+  BG_COLORS,
+  FLEX_DIRECTION,
+  FONT,
+  FONT_SIZES,
+  JUSTIFY,
+  TEXT_COLORS,
+} from "src/constants";
 
-export type BoxType = {};
+export type BoxType = {
+  width?: string;
+  height?: string;
+  bg?: typeof BG_COLORS[number];
+  border?: string;
+  radius?: string;
+  justify?: typeof JUSTIFY[number];
+  align?: typeof ALIGN[number];
+  flex?: string;
+  flexDirection?: typeof FLEX_DIRECTION[number];
+  margin?: string;
+  padding?: string;
+  className?: string;
+  children?: ReactNode;
+  onClick?: MouseEventHandler<HTMLDivElement>;
+};
 
-export type ButtonType = {};
+export type FlexType = BoxType & {};
+
+export type ButtonType = BoxType & {
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+};
 
 export type CardType = {};
-
-export type FlexType = {};
 
 export type IconType = {};
 
@@ -18,11 +44,16 @@ export type TextInputType = {};
 
 export type ParagraphType = {};
 
-export type TextType = {};
+export type TextType = Pick<BoxType, "className" | "children"> & {
+  onClick?: MouseEventHandler<HTMLSpanElement>;
+  font?: typeof FONT[number];
+  color?: typeof TEXT_COLORS[number];
+  fontSize?: typeof FONT_SIZES[number];
+};
 
 export type HeadType = {};
 
 export type FontType = {
   children: ReactNode;
-  type: "Manrope" | "Rebond";
+  type: typeof FONT[number];
 };
